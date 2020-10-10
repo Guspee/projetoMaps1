93cf83aca839926fa7622f8bef6fd74c777cab00
Guspee <guandreas90@hotmail.com>
2020-10-09 21:13:16 -0300

    aaa

---
+++ app/src/main/java/com/example/projetomaps/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.projetomaps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private static final int REQUEST_COD_GPS = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+
+            }
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras){
+
+            }
+            @Override
+            public void onProviderEnabled(String provide){
+
+            }
+            @Override
+            public void onProviderDisabled(String provider){
+
+            }
+        };
+
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }else{
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_COD_GPS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        if(requestCode == REQUEST_COD_GPS){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0 , 0 , locationListener);
+                }
+            }
+        }else{
+            Toast.makeText(this, getString(R.string.no_gps_no_app), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    public void carregarMapa(View view){
+
+    }
+}
